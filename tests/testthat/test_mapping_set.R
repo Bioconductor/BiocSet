a4b42206fe83d0221cbf5db7683d2c52748aa8d1
Kayla-Morrell <Kayla.Morrell@roswellpark.org>
2020-06-04 13:27:12 -0400

    Bug fixes to tests
    - Due to the new version of GO.db, tests were failing
    - Now all tests pass

--- tests/testthat/test_mapping_set.R
+++ tests/testthat/test_mapping_set.R
@@ -6,33 +6,33 @@ test_that("'go_sets()' works",
     es <- go_sets(org.Hs.eg.db, "ENSEMBL")
 
     expect_s4_class(es, "BiocSet")
-    expect_gte(dim(es_element(es))[1], 22512L)
+    expect_gte(dim(es_element(es))[1], 22432L)
     expect_identical(dim(es_element(es))[2], 1L)
-    expect_gte(dim(es_set(es))[1], 18175L)
+    expect_gte(dim(es_set(es))[1], 18105L)
     expect_identical(dim(es_set(es))[2], 1L)
-    expect_gte(dim(es_elementset(es))[1], 318556L)
+    expect_gte(dim(es_elementset(es))[1], 317035L)
     expect_identical(dim(es_elementset(es))[2], 2L)
     expect_true(.is_tbl_elementset(es_elementset(es)))
 
     es1 <- go_sets(org.Hs.eg.db, "SYMBOL", evidence = c("IDA", "IMP", "HDA"))
 
     expect_s4_class(es1, "BiocSet")
-    expect_gte(dim(es_element(es1))[1], 13167L)
+    expect_gte(dim(es_element(es1))[1], 13163L)
     expect_identical(dim(es_element(es1))[2], 1L)
-    expect_gte(dim(es_set(es1))[1], 11117L)
+    expect_gte(dim(es_set(es1))[1], 11069L)
     expect_identical(dim(es_set(es1))[2], 2L)
-    expect_gte(dim(es_elementset(es1))[1], 87788L)
+    expect_gte(dim(es_elementset(es1))[1], 87445L)
     expect_identical(dim(es_elementset(es1))[2], 2L)
     expect_true(.is_tbl_elementset(es_elementset(es1)))
     
     es2 <- go_sets(org.Hs.eg.db, "SYMBOL", ontology = "MF")
 
     expect_s4_class(es2, "BiocSet")
-    expect_gte(dim(es_element(es2))[1], 17697L)
+    expect_gte(dim(es_element(es2))[1], 17696L)
     expect_identical(dim(es_element(es2))[2], 1L)
-    expect_gte(dim(es_set(es2))[1], 4165L)
+    expect_gte(dim(es_set(es2))[1], 4140L)
     expect_identical(dim(es_set(es2))[2], 2L)
-    expect_gte(dim(es_elementset(es2))[1], 65158L)
+    expect_gte(dim(es_elementset(es2))[1], 64664L)
     expect_identical(dim(es_elementset(es2))[2], 2L)
     expect_true(.is_tbl_elementset(es_elementset(es2)))
 
@@ -93,7 +93,7 @@ test_that("'map_add_set()' works",
     go <- go_sets(org.Hs.eg.db, "ENSEMBL")
     map <- map_add_set(go, GO.db, "GOID", "DEFINITION")
 
-    expect_gte(length(map), 18175L)
+    expect_gte(length(map), 18105L)
     expect_identical(class(map), "character")
 
     expect_error(map_add_set(GO.db, "GOID", "DEFINITION"))
