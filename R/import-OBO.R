46e0d013dd259b4a5a1d5fddfaf14ed582660109
Martin Morgan <Martin.Morgan@RoswellPark.org>
2020-07-16 04:15:07 -0400

    refactor import-OBO
    - elementset table has is_a column
    - more work required

--- R/import-OBO.R
+++ R/import-OBO.R
@@ -3,81 +3,95 @@
 OBOFile = function(resource, ...)
     .OBOFile(resource = resource)
 
+## Need to account for diff format versions, order and tags can change
+## These tags are for format version 1.2
+.OBO_TAGS <- list(
+    "1.2" = list(
+        TERMS = c(
+            "id", "is_anonymous", "name", "namespace", "alt_id", "def",
+            "comment", "subset", "synonym", "xref", "is_a", "intersection_of",
+            "union_of", "disjoint_from", "relationship", "is_obsolete",
+            "replaced_by", "consider", "created_by", "creation_date"
+        )
+    )
+)
+
+.obo_term_tags <- function(version) {
+    if (!version %in% names(.OBO_TAGS)) {
+        warning("unknown OBO version '", version, "'; using version 1.2")
+        version <- "1.2"
+    }
+    .OBO_TAGS[[version]]$TERMS
+}
+
 #' @importFrom ontologyIndex get_ontology
 #' @importFrom tibble as_tibble tibble
 #' @importFrom dplyr rename anti_join
 .import_obo <- function(path, extract_tags = "minimal") {
-    stopifnot(extract_tags %in% c("minimal", "everything"))
+    stopifnot(
+        extract_tags %in% c("minimal", "everything")
+    )
     
-    ## Need to account for diff format versions, order and tags can change
-    ## These tags are for format version 1.2
-    term_tags <- c(
-        "id", "is_anonymous", "name", "namespace", "alt_id", "def",
-        "comment", "subset", "synonym", "xref", "is_a", "intersection_of",
-        "union_of", "disjoint_from", "relationship", "is_obsolete",
-        "replaced_by", "consider", "created_by", "creation_date"
-    )    
-    if (extract_tags == "everything") {
-        obo <- get_ontology(path, extract_tags = "everything")
-        elements <- as_tibble(lapply(obo, as.vector)) %>%
-            filter(!.data$namespace %in% "external") %>%
-            rename(element = "id")
-        info_columns <- elements[-c(1:6)]
-        if (!all(names(info_columns) %in% term_tags)) {
-            keep <- names(elements)[names(elements) %in% term_tags]
-            drop <- names(info_columns)[!names(info_columns) %in% term_tags]
-            elements <- elements %>% select(1:6, keep)
-            cat("The columns:\n", 
-                drop, 
-                "\nhave been dropped from es_element.\n",
-                sep = "\n"
-            )
-        }
-    } else {
-        obo <- get_ontology(path)
-        elements <- as_tibble(lapply(obo, as.vector)) %>%
-            filter(grepl("GO:", .data$id, fixed = TRUE)) %>%
-            rename(element = "id")
-    }
-
-    sets <-
-        elements %>%
-        filter(lengths(.data$children) > 0) %>%
-        select("element", "name", "parents", "children", "ancestors") %>%
-        rename(set = "element")
-
-    myunnest <- function(tbl) {
-        tibble(
-            set = rep(tbl$set, lengths(tbl$element)),
-            element = unlist(tbl$element)
-        )
-    }
-
-    elementsets <-
-        elements %>%
-        filter(lengths(.data$children) > 0L) %>%
-        rename(set = "element") %>%
-        select("set", element = "children") %>%
-        myunnest()
-
-    ## elements not in any set are in their own set
-    identity <-
-        elements %>%
-        select("element") %>%
-        anti_join(elementsets, by = "element") %>%
-        mutate(set = .data$element)
-    elementsets <-
-        elementsets %>%
-        bind_rows(identity)
+    obo <- get_ontology(path, extract_tags = "everything")
 
     ## header
-    version <- attr(obo, "version")
+    version0 <- attr(obo, "version")
     pattern <- "^([^:]+):(.*)"
     obo_header <- tibble(
-        key = sub(pattern, "\\1", version),
-        value = trimws(sub(pattern, "\\2", version))
+        key = sub(pattern, "\\1", version0),
+        value = trimws(sub(pattern, "\\2", version0))
     )
+    version <-
+        obo_header %>%
+        filter(key == "format-version") %>%
+        pull("value")
     metadata <- list(obo_header = obo_header)
+
+    ## parent / child ancestor information
+    structure <- c("id", "parents", "children", "ancestors", "namespace")
+    isa <-
+        obo[intersect(names(obo), structure)] %>%
+        lapply(function(x) unname(as.vector(x))) %>%
+        as_tibble() %>%
+        rename(element = "id")
+
+    if ("namespace" %in% names(isa))
+        isa <- isa %>% filter(.data$namespace != "external")
+
+    ## elements
+    minimal <- c("element", "name", "obsolete")
+    tags <- c(minimal, .obo_term_tags(version))
+    elements <-
+        obo[intersect(names(obo), tags)] %>%
+        lapply(function(x) unname(as.vector(x))) %>%
+        as_tibble() %>%
+        rename(element = "id")
+
+    if ("namespace" %in% names(elements))
+        elements <- elements %>% filter(.data$namespace != "external")
+
+    if (extract_tags == "minimal")
+        elements <- elements %>% select(minimal)
+
+    ## sets
+    sets <-
+        elements %>%
+        select(set = "element", "name")
+
+    ## elementsets
+    child <-
+        isa %>%
+        select(element = "element", set = "parents") %>%
+        unnest("set") %>%
+        mutate(is_a = TRUE)
+    offspring <-
+        isa %>%
+        select(set = "ancestors", element = "element") %>%
+        unnest("set")
+    elementsets <-
+        right_join(child, offspring, by = c("element", "set")) %>%
+        mutate(is_a = !is.na(.data$is_a)) %>%
+        select("element", "set", "is_a")
 
     OBOSet(elementsets, elements, sets, metadata)
 }
@@ -143,18 +157,35 @@ oboset_set_ancestors <- function(oboset) {
         unnest("ancestors")
 }
 
+.oboset_is_a <- function(oboset) {
+    isa <-
+        es_elementset(oboset) %>%
+        filter(.data$is_a) %>%
+        select("element", "set")
+    split(isa$element, isa$set) %>%
+        tibble(element = names(.)) %>%
+        select(element, is_a = ".") %>%
+        mutate(is_a = unname(is_a))
+}
+
 .export_obo <- function(tbl, path) {
     stopifnot(
         .is_tbl_elementset(es_elementset(tbl)),
         `'path' exists` = !file.exists(path)
     )
     
-    elements <- es_element(tbl)
-    if (!"id" %in% names(elements))
-        elements <- elements %>% rename(id = "element")
+    version <-
+        metadata(tbl)[["obo_header"]] %>%
+        filter(.data$key == "format-version") %>%
+        pull(value)
+    term_tags <- .obo_term_tags(version)
 
-    if (!"is_a" %in% names(elements))
-        elements <- elements %>% rename(is_a = "parents")
+    elements <- es_element(tbl)
+
+    if (!"is_a" %in% names(elements)) {
+        isa <- .oboset_is_a(tbl)
+        elements <- elements %>% left_join(isa, by = "element")
+    }
 
     if ("obsolete" %in% names(elements) & is.logical(elements$obsolete)) {
         elements <-
@@ -163,20 +194,14 @@ oboset_set_ancestors <- function(oboset) {
             mutate(is_obsolete = as.integer(.data$is_obsolete))
     }
 
+    if (!"id" %in% names(elements))
+        elements <- elements %>% rename(id = "element")
+
     ## order elements by id
     elements <- elements %>% arrange(.data$id)
 
-    ## Need to account for diff format versions, order and tags can change
-    ## These tags are for format version 1.2
-    term_tags <- c(
-        "id", "is_anonymous", "name", "namespace", "alt_id", "def",
-        "comment", "subset", "synonym", "xref", "is_a", "intersection_of",
-        "union_of", "disjoint_from", "relationship", "is_obsolete",
-        "replaced_by", "consider", "created_by", "creation_date"
-    )
     ## keep only known columns
     elements <- elements[names(elements) %in% term_tags]
-
 
     ## term-key-value tibble
     kv <- tibble(
