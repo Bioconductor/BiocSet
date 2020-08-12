c2539d00a816eb64b6b2452bb43fe5c1c2c909b4
Kayla-Morrell <kayla.morrell@roswellpark.org>
2020-08-12 14:39:04 -0400

    Addition of documentation
    - Documenting the two `GeneSetCollection` related functions

--- R/import-GeneSetCollection.R
+++ R/import-GeneSetCollection.R
@@ -1,3 +1,16 @@
+#' GeneSetCollection
+#' @rdname genesetcollection
+#' @name genesetcollection
+#' @description The following functions deal with converting a \code{BiocSet}
+#'     object into a \code{GeneSetCollection} object, or vice versa.
+#' @param biocset The \code{BiocSet} object that will become a
+#'     \code{GeneSetCollection} object.
+#' @return For `GeneSetCollection_from_BiocSet()`, a GeneSetCollection.
+#' @export
+#' @examples
+#' biocset <- BiocSet(set1 = letters, set2 = LETTERS)
+#' gsc <- GeneSetCollection_from_BiocSet(biocset)
+#' gsc
 GeneSetCollection_from_BiocSet <- function(biocset) {
     if(!requireNamespace("GSEABase", quietly = TRUE)) {
         stop("Please install GSEABase")
@@ -10,6 +23,14 @@ GeneSetCollection_from_BiocSet <- function(biocset) {
     GSEABase::GeneSetCollection(gsl)
 }
 
+#' @rdname genesetcollection
+#' @param gsc The \code{GeneSetCollection} that will become a \code{BiocSet}
+#'     object.
+#' @return For `BiocSet_from_GeneSetCollection()`, a BiocSet object.
+#' @export
+#' @examples
+#'
+#' BiocSet_from_GeneSetCollection(gsc)
 BiocSet_from_GeneSetCollection <- function(gsc) {
     if(!requireNamespace("GSEABase", quietly = TRUE)) {
         stop("Please install GSEABase")
